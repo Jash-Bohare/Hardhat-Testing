9273d44350bec1cf6681ec79c4e265e98927930b
Jash Bohare <jashbohare@gmail.com>
2026-01-19 19:03:17 +0530

    Formatted Counter.t.js

--- Test/Counter.t.js
+++ Test/Counter.t.js
@@ -1,28 +1,28 @@
-const {expect} = require("chai");
+const { expect } = require("chai");
 
-describe("Counter Contract", function (){
+describe("Counter Contract", function () {
     let Counter;
     let counter;
     const INITIAL_VALUE = 10;
 
-    beforeEach(async function(){
+    beforeEach(async function () {
         Counter = await ethers.getContractFactory("Counter");
         counter = await Counter.deploy(INITIAL_VALUE);
     });
 
-    describe("Deployment", function(){
-        it("Should initialize the value", async function(){
+    describe("Deployment", function () {
+        it("Should initialize the value", async function () {
             expect(await counter.get()).to.equal(INITIAL_VALUE);
         });
     });
 
-    describe("Increment", function(){
-        it("Should increment the value by 1", async function(){
+    describe("Increment", function () {
+        it("Should increment the value by 1", async function () {
             await counter.increment();
             expect(await counter.get()).to.equal(INITIAL_VALUE + 1);
         });
 
-        it("Should allow multiple increments", async function(){
+        it("Should allow multiple increments", async function () {
             await counter.increment();
             await counter.increment();
             await counter.increment();
@@ -30,13 +30,13 @@ describe("Counter Contract", function (){
         });
     });
 
-    describe("Decrement", function(){
-        it("Should decrement the value by 1", async function(){
+    describe("Decrement", function () {
+        it("Should decrement the value by 1", async function () {
             await counter.decrement();
             expect(await counter.get()).to.equal(INITIAL_VALUE - 1);
         })
 
-        it("Should revert when value is zero", async function(){
+        it("Should revert when value is zero", async function () {
             const zeroCounter = await Counter.deploy(0);
             await expect(zeroCounter.decrement()).to.be.revertedWith("Counter: below zero");
         });
